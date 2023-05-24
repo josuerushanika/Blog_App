508fb4f87c143ac574f0188200957ac5f4c4ee02
josuerushanika <josuerushanika@gmail.com>
2023-05-24 23:53:50 +0200

    Update files and fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    after_create :update_author_posts_counter
-    
-    private
+  after_create :update_author_posts_counter
 
-    def update_post_likes_counter
-        post.increment!(:likes_counter)
-    end
+  private
+
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-   has_many: posts, foreign_key: author_id
-   has_many: comments, foreign_key: author_id
-   has_many: likes, foreign_key: author_id
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
 
-   def recent_posts
-     posts.order(created_at: :desc).limit(5)
-   end
+  def recent_posts
+    posts.order(created_at: :desc).limit(5)
+  end
 
-   private 
+  private
 
-   def update_author_posts_counter
-     author.increment!(:posts_counter)
-   end
+  def update_author_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
