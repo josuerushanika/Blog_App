49e23e91562d47245a61c2b382a2bcec155f98df
josuerushanika <josuerushanika@gmail.com>
2023-05-24 23:07:55 +0200

    Update like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,10 @@
 class Like < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    private
+
+    def update_post_likes_counter
+        post.increment!(:likes_counter)
+    end
 end
