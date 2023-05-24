8a8e2a4073c21e459041a9fc73eb825698aca388
josuerushanika <josuerushanika@gmail.com>
2023-05-24 23:23:13 +0200

    Update change

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    private
+
+    def update_post_comments_counter
+        post.increment!(:comments_counter)
+    end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
 
+    after_create :update_author_posts_counter
+    
     private
 
     def update_post_likes_counter
