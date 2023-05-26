002dbbedf7b84b6b35d932ea2b0472f5eb9b3d31
josuerushanika <josuerushanika@gmail.com>
2023-05-26 14:28:27 +0200

    Update like spec

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,12 +2,11 @@ class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
   
-    after_create :increment_like_counter
+    after_create :increase_like_counter
   
-    private
-  
-    def increment_like_counter
-      self.post.increment!(:likes_counter)
+    def increase_like_counter
+      post.increment!(:likes_counter)
     end
   end
+  
   
--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -12,20 +12,21 @@ RSpec.describe Like, type: :model do
     expect(like.post_id).to_not eql(-5)
     expect(like.post_id).to eql(1)
   end
-
+   
   it 'increases likes_counter for the associated post' do
-    user = User.create(name: 'Mert', photo: 'www.unsplash.com', bio: 'Lorem ipsum', posts_counter: 5)
-    post = Post.create(author_id: 1, title: 'first post', text: 'this is the first post', comments_counter: 3, likes_counter: 2)
-
-    like = Like.create(author_id: 1, post_id: post.id)
-
-    post.likes_counter = 2
+    User.new(name: 'Mert', photo: 'www.unsplash.com', bio: 'Lorem ipsum', posts_counter: 5)
+  
+    post = Post.new(author_id: 1, title: 'first post', text: 'this is the first post', comments_counter: 3, likes_counter: 2)
     post.save
-
+  
+    like = Like.new(author_id: 1, post: post)
+    like.save
+  
     like.increase_like_counter
-    like.increase_like_counter
-
-    expect(post.reload.likes_counter).to eq(4)
+  
+    expect(post.likes_counter).to eq(3)
   end
+  
+ 
 end
 
