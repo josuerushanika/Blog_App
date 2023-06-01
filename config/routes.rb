596d154d3f0404846b609512325ff9d76d2e6d1b
josuerushanika <josuerushanika@gmail.com>
2023-06-01 23:40:45 +0200

    Update router and posts view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end  
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
 end
+
