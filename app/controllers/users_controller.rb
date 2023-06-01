4845defa91af97f446e25eb4222c5ea38c475037
josuerushanika <josuerushanika@gmail.com>
2023-06-01 22:07:25 +0200

    Update files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @post.author = current_user
+
+    if @post.save
+      redirect_to "/users/#{current_user.id}/posts", notice: 'Post created Successfully'
+    else
+      render :new, notice: 'Post not created'  
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,5 @@
 # The base class for all models in the application.
 # Provides common functionality and configuration for models.
 class ApplicationRecord < ActiveRecord::Base
-    def current_user
-      @current_user = User.first
-    end
+  primary_abstract_class
 end
