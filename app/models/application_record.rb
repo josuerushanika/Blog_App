df0c2c27cf440e810ebd8d8052370b1eef5d3a92
josuerushanika <josuerushanika@gmail.com>
2023-06-01 19:40:23 +0200

    Add method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,7 @@
 # The base class for all models in the application.
 # Provides common functionality and configuration for models.
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+    def current_user
+      @current_user = User.first
+    end
 end
