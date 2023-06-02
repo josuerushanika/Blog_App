4ebe507472f21ac53a7c5f2784a242388d3eb636
josuerushanika <josuerushanika@gmail.com>
2023-06-02 12:46:08 +0200

    Update post-controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,29 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to "/users/#{current_user.id}/posts", notice: 'Post created Successfully'
+    else
+      flash[:alert] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
